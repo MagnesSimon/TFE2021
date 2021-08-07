bb9b36ed8474039a698b13036014a7ad05c0091a
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2021-08-07 10:16:18 +0200

    retour à 0 sur la liste des pièce en stock

--- ERP/frontend/src/app/app.module.ts
+++ ERP/frontend/src/app/app.module.ts
@@ -2,42 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ListeViewComponent } from './liste-view/liste-view.component';
 import {RouterModule, Routes} from "@angular/router";
 import { HeaderComponent } from './header/header.component';
 import { AutentificationComponent } from './autentification/autentification.component';
 import { HistoriqueComponent } from './historique/historique.component';
 import { DroitsComponent } from './droits/droits.component';
 import { PenurieComponent } from './penurie/penurie.component';
-import {PieceServices} from "./services/piece.services";
 import {HttpClientModule} from "@angular/common/http";
+import {VariableGlobale} from "./variableGlobale";
+import {FormsModule} from "@angular/forms";
+import { StockComponent } from './stock/stock.component';
 
 const appRoutes: Routes = [
-  {path: 'liste' , component : ListeViewComponent},
+  {path: 'stock' , component: StockComponent },
   {path: 'autentification', component : AutentificationComponent},
   {path: 'penurie', component : PenurieComponent},
   {path: 'historique', component : HistoriqueComponent},
   {path: 'droits', component : DroitsComponent},
-  {path: '', component : ListeViewComponent}
+  {path: '', component: StockComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListeViewComponent,
     HeaderComponent,
     AutentificationComponent,
     HistoriqueComponent,
     DroitsComponent,
-    PenurieComponent
+    PenurieComponent,
+    StockComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
-    PieceServices
+    VariableGlobale
   ],
   bootstrap: [AppComponent]
 })
---
+++ ERP/frontend/src/app/models/stock.model.ts
@@ -0,0 +1,12 @@
+export class Stock {
+
+  constructor(public reference: string,
+              public valeurSeuil: string,
+              public quantiteEnStock: string,
+              public nomFamille: string,
+              public nomCategorie : string,
+              public nomFinition : string,
+              public effetFinition : string
+  ) {
+  }
+}
